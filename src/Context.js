43eec4e6b786bba5ec2e31561c721e9a7ba96c44
Alvaro <alvaroms_25@hotmail.com>
2020-08-18 19:54:35 +0200

    first commit

---
+++ src/Context.js
@@ -0,0 +1,23 @@
+
+class CommandContext{
+    constructor(Bot, message){
+        this.bot = Bot;
+        this.message = message;
+        this.guild = message.guild;
+        this.channel = message.channel;
+        this.author = message.author;
+        
+    }
+
+    async send(args){
+        await this.channel.send(args);
+    }
+    
+    async reply(args){
+        await this.channel.send(`${this.author.mention}, ${args}`);
+    }
+
+    async direct(args){
+        await this.author.send(args);
+    }
+}
