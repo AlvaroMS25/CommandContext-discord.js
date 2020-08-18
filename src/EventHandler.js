dff6ae82db4be8a9d471723331ea494dcd46f261
Alvaro <alvaroms_25@hotmail.com>
2020-08-18 23:34:31 +0200

    :v

--- src/EventHandler.js
+++ src/EventHandler.js
@@ -4,11 +4,10 @@ const path = require('path');
 class EventHandler{
     constructor(bot, dir){
         this.bot = bot;
-        this.dir = dir;
     }
 
-    async Start(){
-        let files = fs.readdirSync(this.dir).filter((f) => f.endsWith('.js'));
+    async Start(dir){
+        let files = fs.readdirSync(dir).filter((f) => f.endsWith('.js'));
         files.forEach(file => {
             let eventName = file.substring(0, file.indexOf(".js"));
             let file_path = path.join(this.dir, file)
