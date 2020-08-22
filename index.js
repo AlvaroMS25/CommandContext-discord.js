91f5594052aa1a74ebb7f15f0beff0f8c22e63cb
Alvaro <alvaroms_25@hotmail.com>
2020-08-22 20:07:12 +0200

    fixed a bug

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const {CommandContext} = require('./src/CommandContext');
 const {CommandHandler} = require('./src/CommandHandler');
 const {CommandExecutor} = require('./src/CommandExecutor');
 const {EventHandler} = require('./src/EventHandler');
+const {SQLExecutor} = require('./src/SQLExecutor');
 
 module.exports = {
     CommandContext: CommandContext,
     CommandHandler: CommandHandler,
     EventHandler: EventHandler,
-    CommandExecutor: CommandExecutor
+    CommandExecutor: CommandExecutor,
+    SQLExecutor: SQLExecutor
 }
